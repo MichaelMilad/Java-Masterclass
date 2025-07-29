bc0c6550a779d0b619b9ac6d28bc988203cb453f
Michael Milad <michaelmilad001@gmail.com>
2025-07-29 22:25:37 +0300

    update

--- OOP_Principles/Polymorphism/src/Main.java
+++ OOP_Principles/Polymorphism/src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie theMovie = Movie.getMovie("Sucker", "Star Wars");
+        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.print(
+                    "Enter Type: \n(A) for Adventure \n(C) for Comedy \n(S) " +
+                            "for Science Fiction \n(Q) to quit\n");
+            String type = s.nextLine();
+
+            if(type.equalsIgnoreCase("q")) break;
+
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+    }
 }
--- OOP_Principles/Polymorphism/src/NextMain.java
+++ OOP_Principles/Polymorphism/src/NextMain.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class NextMain {
+    public static void main(String[] args) {
+//        Movie movie = Movie.getMovie("A", "Jaws");
+//        movie.watchMovie();
+//
+//        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+//        jaws.watchMovie();
+//
+//        Object comedy = Movie.getMovie("C", "Airplane");
+//        Comedy comedyMovie = (Comedy) comedy;
+//        comedyMovie.watchComedy();
+//
+//        var airplane = Movie.getMovie("C", "Airplane");
+//        airplane.watchMovie();
+
+        Object unknownObject = Movie.getMovie("Scaa", "airplance");
+        if (unknownObject.getClass().getSimpleName() == "Comedy") {
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if (unknownObject instanceof Adventure) {
+            ((Adventure) unknownObject).watchAdventure();
+        } else if (unknownObject instanceof ScienceFiction syfy) {
+            syfy.watchScienceFiction();
+        }
+    }
 }
